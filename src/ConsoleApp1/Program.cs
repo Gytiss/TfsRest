6e79fdb75db04caf6b92cbcbf0fd319b630eaedf
Gytis Griskinas <ggriskinas@googlemail.com>
2017-02-20 00:17:33 +0200

    second commit

--- src/ConsoleApp1/Program.cs
+++ src/ConsoleApp1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace ConsoleApp1
 {
@@ -9,6 +10,43 @@ namespace ConsoleApp1
     {
         public static void Main(string[] args)
         {
+
+        }
+
+        public static async void GetProjects()
+        {
+            try
+            {
+                var builder = new ConfigurationBuilder()
+                     .AddJsonFile("project.json");
+
+                var configuration = builder.Build();
+                var setting = configuration["userSecretsId"];
+
+                using (HttpClient client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Accept.Add(
+                        new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
+                        Convert.ToBase64String(
+                            System.Text.ASCIIEncoding.ASCII.GetBytes(
+                                string.Format("{0}:{1}", "", setting))));
+
+                    using (HttpResponseMessage response = client.GetAsync(
+                                "https://autotest1.VisualStudio.com/DefaultCollection/_apis/projects?api-version=2.0").Result)
+                    {
+                        response.EnsureSuccessStatusCode();
+                        string responseBody = await response.Content.ReadAsStringAsync();
+                        Console.WriteLine(responseBody);
+                        Console.ReadLine();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
         }
     }
 }
